49d22d3ef6e989c016dd3c1675b06603414c9fcb
blistalicious <nihadibrahimli2@gmail.com>
2025-02-25 18:21:29 +0400

    wrap overlay element logic in DOM content loaded event handler

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,19 @@
-let overlayElement = document.createElement("div");
-overlayElement.style.width = "100%";
-overlayElement.style.height = "100%";
-overlayElement.style.background = "white";
-overlayElement.style.position = "absolute";
-overlayElement.style.top = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  let overlayElement = document.createElement("div");
+  overlayElement.style.width = "100%";
+  overlayElement.style.height = "100%";
+  overlayElement.style.background = "white";
+  overlayElement.style.position = "absolute";
+  overlayElement.style.top = 0;
 
-document.body.style.position = "relative";
-document.body.appendChild(overlayElement);
+  document.body.style.position = "relative";
+  document.body.appendChild(overlayElement);
 
-let clickCount = 0;
-overlayElement.addEventListener("click", function () {
-  clickCount++;
-  if (clickCount === 5) {
-    overlayElement.remove();
-  }
+  let clickCount = 0;
+  overlayElement.addEventListener("click", function () {
+    clickCount++;
+    if (clickCount === 5) {
+      overlayElement.remove();
+    }
+  });
 });
